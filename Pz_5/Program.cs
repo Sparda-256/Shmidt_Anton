ae2f69fcef671a15bf3417c0a3f7d398fbf093a6
Антон Шмидт <sparda89328490316@mail.ru>
2022-12-05 15:46:48 +0500

    Pz_5

--- Pz_5/Program.cs
+++ Pz_5/Program.cs
@@ -1,10 +1,58 @@
-﻿namespace Pz_7
+﻿using System;
+
+namespace Pz_5
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Введите номер задания (1-5): ");
+            int task = Convert.ToInt32(Console.ReadLine());
+
+            switch (task)
+            {
+                case 1:
+                    for (int i = 0; i < 16; i++)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    break;
+                case 2:
+                    for (char bukva = 'о'; bukva != 'я'; bukva++)
+                    {
+                        Console.Write(bukva);
+                    }
+                    Console.WriteLine('я');
+                    break;
+                case 3:
+                    for (int m = 0; m < 10; ++m)
+                    {
+                        for (int n = 0; n < 5; ++n)
+                        {
+                            Console.Write('#');
+                        }
+                        Console.Write('\n');
+                    }
+                    break;
+                case 4:
+                    for (int i = 0; i <= 100; ++i)
+                    {
+                        if (i % 16 == 0)
+                        {
+                            Console.WriteLine(i);
+                        }
+                    }
+                    break;
+                case 5:
+                    for (int i = 0, j = 30; j - i >= 8; ++i, --j)
+                    {
+                        Console.WriteLine($"i = {i},\tj = {j}");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Неверное значение! Пробуйте снова");
+                    break;
+            }
         }
     }
 }
