cad9e818ae2905d23d9c9a8393b09989b381c531
Антон Шмидт <sparda89328490316@mail.ru>
2022-12-05 12:26:57 +0500

    Pz_6

---
+++ Pz_6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int end = 4;
+            double counter = 1;
+            while (counter <= end)
+            {
+                Console.WriteLine(counter + 3 * Math.Pow(counter, 2) - 2.5 * Math.Pow(counter, 3) + 0.75 * Math.Pow(counter, 4));
+                counter += 0.5;
+            }
+        }
+    }
+}
