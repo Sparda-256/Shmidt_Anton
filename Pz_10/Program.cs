71c91d1fd21d862a018d771cc19c08ad143d9201
Антон Шмидт <sparda89328490316@mail.ru>
2022-12-14 12:22:18 +0500

    Pz_10

---
+++ Pz_10/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Pz_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stroka = "";
+            int a = 1;
+            int b = 0;
+            while (a != 0)
+            {
+                stroka = Console.ReadLine();
+                a = Convert.ToInt32(stroka.IndexOf("_"));
+                b += 1;
+            }
+            Console.WriteLine("Вывод:");
+            for (int i = 0; i < b; i++)
+            {
+                Console.WriteLine(stroka);
+            }
+        }
+    }
+}
