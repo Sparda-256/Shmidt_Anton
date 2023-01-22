1c4c3fb9342ee0d11d284fa84bc85c388fdfd5af
Антон Шмидт <sparda89328490316@mail.ru>
2023-01-22 23:47:15 +0500

    Pz_20

---
+++ Животное/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Животное
+{
+    internal class Klinika
+    {
+        public string typeofanimal = "cat"; //тип животного
+        public string name = "Marusya"; //кличка
+        public string hozain = "Anton"; //хозяин
+        public string problems = "pain"; //жалобы
+
+        internal void information()
+        {
+            Console.WriteLine($"Тип животного {typeofanimal}\n"
+                + $"Кличка {name}\n"
+                + $"Хозяин {hozain}\n"
+                + $"Жалобы {problems}\n");
+        }
+    }
+}
