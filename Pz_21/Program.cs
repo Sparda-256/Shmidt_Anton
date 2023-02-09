7f6695d8572a9a68a748b2c3084db14665edefdf
Антон Шмидт <sparda89328490316@mail.ru>
2023-02-09 13:26:39 +0500

    Pz_21 add

---
+++ Pz_21/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Pz_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime Date = Convert.ToDateTime("09/02/2023");
+            Student student = new Student();
+            Console.Write("Имя: "); string A = student.name = Console.ReadLine();
+            Console.Write("Фамилия: "); string B = student.surname = Console.ReadLine();
+            Console.Write("Отчество: "); string C = student.patronymic = Console.ReadLine();
+            Console.Write("Дата рождения: "); string D = student.dateOfBirth = Console.ReadLine();
+            Console.Write("Год начала обучения: "); int E = student.yearOfStudy = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Специальность: "); string F = student.speciality = Console.ReadLine();
+            
+            student.PrintInfo(A, B, C, D, E, F);
+
+            DateTime hirthday = new DateTime(2020, 10, 10);
+            DateTime hirthday2 = new DateTime(E);
+            Console.WriteLine(hirthday2.Year(hirthday));
+            student.GetCourse() ;
+        }
+    }
+}
---
+++ Pz_21/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+
+namespace Pz_21
+{
+    internal class Student
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string patronymic { get; set; }
+        public string dateOfBirth { get; set; }
+        public int yearOfStudy { get; set; }
+        public string speciality { get; set; }
+        public void GetCourse(int date)
+        {
+            Console.WriteLine($"Вы учислись: {date}");
+        }
+        public void PrintInfo(string name, string surname, string patronymic, string dateOfBirth, int yearOfStudy, string speciality)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Студент:\n{name}\n{surname}\n{patronymic}\n{dateOfBirth}\n{yearOfStudy}\n{speciality}");
+        }
+    }
+}
