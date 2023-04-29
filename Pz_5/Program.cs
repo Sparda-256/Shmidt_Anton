3a9de143cd1a6ae83fbd8e78b12e744e91965f9c
Антон Шмидт <sparda89328490316@mail.ru>
2023-04-29 18:45:17 +0500

    Pz_5

--- Pz_5/Program.cs
+++ Pz_5/Program.cs
@@ -13,44 +13,31 @@ namespace Pz_5
             {
                 case 1:
                     for (int i = 0; i < 16; i++)
-                    {
                         Console.WriteLine(i);
-                    }
                     break;
                 case 2:
-                    for (char bukva = 'о'; bukva != 'я'; bukva++)
-                    {
+                    for (char bukva = 'о'; bukva != 'ф'; bukva++)
                         Console.Write(bukva);
-                    }
-                    Console.WriteLine('я');
                     break;
                 case 3:
-                    for (int m = 0; m < 10; ++m)
+                    for (int m = 0; m < 10; m++)
                     {
-                        for (int n = 0; n < 5; ++n)
-                        {
-                            Console.Write('#');
-                        }
+                        for (int n = 0; n < 4; n++)
+                        Console.Write('#');
                         Console.Write('\n');
                     }
                     break;
                 case 4:
-                    for (int i = 0; i <= 100; ++i)
-                    {
-                        if (i % 16 == 0)
-                        {
+                    for (int i = 0; i <= 100; i++)
+                        if (i % 14 == 0)
                             Console.WriteLine(i);
-                        }
-                    }
                     break;
                 case 5:
-                    for (int i = 0, j = 30; j - i >= 8; ++i, --j)
-                    {
+                    for (int i = 0, j = 30; j - i >= 8; i++, j--)
                         Console.WriteLine($"i = {i},\tj = {j}");
-                    }
                     break;
                 default:
-                    Console.WriteLine("Неверное значение! Пробуйте снова");
+                    Console.WriteLine("Неверное значение!");
                     break;
             }
         }
