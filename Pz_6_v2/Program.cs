0fd4024153ddc9b1fc8c93bbc803adb37cef52fd
Антон Шмидт <sparda89328490316@mail.ru>
2023-04-29 19:25:22 +0500

    Pz_6_v2

---
+++ Pz_6_v2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Pz_6_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float v, g, t;
+            t = 0;
+            g = 9.8f;
+            v = 0;
+            while (v < 1000)
+            {
+                v = g * t;
+                Console.WriteLine(v);
+                t += 0.5f;
+            }
+        }
+    }
+}
