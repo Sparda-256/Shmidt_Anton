2feb081b8c48fda1e3c00f4aaea2921445ce09d9
Антон Шмидт <sparda89328490316@mail.ru>
2023-04-29 22:25:28 +0500

    Pz_7_v2

---
+++ Pz_7_v2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml;
+
+namespace Pz_7_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] masiv = new int [20];
+            Random random = new Random();
+            int max = 0;
+            for (int i = 0; i < masiv.Length; i++)
+            {
+                masiv[i] = random.Next(-15, 15);
+                Console.WriteLine(masiv[i]);
+            }
+            for (int i = 0; i < masiv.Length; i++)
+            {
+                if (masiv[i]>max)
+                {
+                    max = masiv[i];
+                }
+            }
+            int count = 0;
+            for (int i = 0; i < masiv.Length; i++)
+            {
+                if (masiv[i] < 0)
+                {
+                    if (Math.Abs(masiv[i]) > max)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine("Максимальный элемент: " + max);
+            Console.WriteLine("Количество элементов по модулю больших, чем максимальный: " + count);
+        }
+    }
+}
