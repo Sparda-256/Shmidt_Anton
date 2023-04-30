88794b5cd1daf15de02157cab265a46c72f2788c
Антон Шмидт <sparda89328490316@mail.ru>
2023-04-30 14:07:34 +0500

    Pz_8_v2

---
+++ Pz_8_v2/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Pz_8_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            float[,] nums = new float[3,5];
+            float min = 0.0f;
+            float summ = 0.0f;
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                for (int j = 0; j < nums.GetLength(1); j++)
+                {
+                    nums[i, j] = random.Next(-20, 21);
+                }
+            }
+            for (int i = 0; i < nums.GetLength(0); i++)
+            {
+                for (int j = 0; j < nums.GetLength(1); j++)
+                {
+                    if (nums[i, j] < min)
+                    {
+                        min = nums[i, j];
+                    }
+                }
+            }
+            foreach (float item in nums)
+            {
+                if (item > 0)
+                {
+                    summ += item;
+                }
+            }
+            Console.WriteLine("Произведение минимального элемента матрицы вещественных чисел" +
+                "на сумму её положительных элементов: " + min * summ);
+        }
+    }
+}
