c3e4753bcfe61dad6b93ec05629e9cf32019d288
Антон Шмидт <sparda89328490316@mail.ru>
2023-04-30 21:39:55 +0500

    Pz_10_v2

---
+++ Pz_10_v2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Pz_10_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь к файлу:");
+            bool proverka = false;
+            string Userinput = Console.ReadLine();
+            while (proverka != true)
+            {
+                if (Userinput[0] != 'C' && Userinput[0] != 'D' || Userinput[1] != ':' || Userinput[2] != '/')
+                {
+                    Console.WriteLine("Вы ввели некорректный путь, попробуйте снова:");
+                    Userinput = Console.ReadLine();
+                }
+                else
+                {
+                    proverka = true;
+                }
+            }
+            Console.WriteLine("Каталоги:");
+            Userinput = Userinput.Remove(0, 3);
+            string[] textArray = Userinput.Split('/');
+            for (int i = 0; i < textArray.Length - 1; i++)
+            {
+                Console.WriteLine(textArray[i]);
+            }
+        }
+    }
+}
