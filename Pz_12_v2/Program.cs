f4c2bf1e9f15cbe72945c70ee5907c1941d3aefd
Антон Шмидт <sparda89328490316@mail.ru>
2023-05-01 15:07:09 +0500

    Pz_12_v2

---
+++ Pz_12_v2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Pz_12_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 5 чисел, мы найдём их 2, 3 и 4 степень:");
+            int[] masiv = new int[5];
+            for (int i = 0; i < masiv.Length; i++)
+            {
+                masiv[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Результат:");
+            for (int i = 0; i < masiv.Length; i++)
+            {
+                Console.WriteLine(PowerA234(masiv[i]));
+            }
+        }
+        public static (int B, int C, int D) PowerA234(int A)
+        {
+            int B, C, D;
+            B = Convert.ToInt32(Math.Pow(A, 2));
+            C = Convert.ToInt32(Math.Pow(A, 3));
+            D = Convert.ToInt32(Math.Pow(A, 4));
+            return (B, C, D);
+        }
+    }
+}
