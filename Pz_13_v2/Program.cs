06f62cc16ca991e785d319cb01074813e57199a7
Антон Шмидт <sparda89328490316@mail.ru>
2023-05-02 10:48:52 +0500

    Pz_13_v2

---
+++ Pz_13_v2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Pz_13_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите периметр комнаты в метрах:");
+            int per = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите высоту стен в метрах:");
+            int steni = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Необходимо рулонов обоев по 1мх10м:");
+            Console.WriteLine(oboi(per, steni));
+        }
+        public static int oboi(int perimetr, int visota)
+        {
+            int rulonov = (perimetr*visota*4)/10;
+            return rulonov;
+        }
+    }
+}
