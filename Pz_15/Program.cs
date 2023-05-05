edd6619acac06a009f3a5bef634296c724d06466
Антон Шмидт <sparda89328490316@mail.ru>
2023-05-06 02:06:46 +0500

    Pz_15

---
+++ Pz_15/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Pz_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = System.IO.File.ReadAllLines(@"C:\Games\bf.txt").Length;
+            string[] ewq = System.IO.File.ReadAllLines(@"C:\Games\bf.txt");
+            for (int i = 0; i < count; i++)
+            {
+                if (ewq[i][0] == ewq[i][ewq[i].Length - 1])
+                {
+                    Console.WriteLine(ewq[i]);
+                }
+            }
+        }
+    }
+}
