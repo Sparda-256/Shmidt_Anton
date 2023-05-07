93c3153c39faeafc224b937d01d04e2c9287e4f5
Антон Шмидт <sparda89328490316@mail.ru>
2023-05-07 13:04:00 +0500

    Pz_17_v2

---
+++ Pz_17_v2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Pz_17_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                Console.Write("Введите строку: ");
+                string input = Console.ReadLine();
+
+                fixed (char* pInput = input)
+                {
+                    int count = 0;
+                    for (char* pChar = pInput; *pChar != '\0'; pChar++)
+                    {
+                        if (*pChar == ' ')
+                        {
+                            count++;
+                        }
+                    }
+                    Console.WriteLine($"Количество пробелов в строке: {count}");
+                }
+            }
+        }
+    }
+}
