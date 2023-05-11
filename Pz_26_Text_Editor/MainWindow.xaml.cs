46897344352010d9435389571f9abe447c711e3a
Антон Шмидт <sparda89328490316@mail.ru>
2023-05-12 00:11:09 +0500

    Pz_26_Text_Editor

---
+++ Pz_26_Text_Editor/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using Microsoft.Win32;
+using System.Windows.Input;
+
+namespace TextEditor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            richTextBox.TextChanged += (sender, e) =>
+            {
+                richTextBox.Document.LineHeight = 0.1 * richTextBox.FontSize;
+            };
+            richTextBox.PreviewKeyDown += richTextBox_PreviewKeyDown;
+        }
+        private void escButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close(); // закрытие окна
+        }
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            // Создать новый документ
+            richTextBox.Document = new FlowDocument();
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            // Открыть диалоговое окно выбора файла
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Открыть файл и загрузить его содержимое в RichTextBox
+                string fileName = openFileDialog.FileName;
+                string fileText = File.ReadAllText(fileName);
+                richTextBox.Document = new FlowDocument(new Paragraph(new Run(fileText)));
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            // Открыть диалоговое окно сохранения файла
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                // Сохранить содержимое RichTextBox в файл
+                string fileName = saveFileDialog.FileName;
+                File.WriteAllText(fileName, new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text);
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            // Удалить содержимое RichTextBox
+            richTextBox.Document = new FlowDocument();
+        }
+        private void richTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Tab)
+            {
+                // Вставляем символ табуляции
+                TextPointer caret = richTextBox.CaretPosition;
+                caret.InsertTextInRun("\t");
+
+                // Перемещаем курсор на следующую позицию
+                richTextBox.CaretPosition = caret.GetPositionAtOffset(1);
+                e.Handled = true;
+            }
+        }
+        private void Italic_Click(object sender, RoutedEventArgs e)
+        {
+            // Переключить курсивное форматирование для выбранного текста
+            var currentValue = richTextBox.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            if (currentValue != DependencyProperty.UnsetValue && currentValue.Equals(FontStyles.Italic))
+            {
+                richTextBox.Selection.ApplyPropertyValue(Inline.FontStyleProperty, FontStyles.Normal);
+            }
+            else
+            {
+                richTextBox.Selection.ApplyPropertyValue(Inline.FontStyleProperty, FontStyles.Italic);
+            }
+        }
+
+        private void Bold_Click(object sender, RoutedEventArgs e)
+        {
+            // Переключить жирное форматирование для выбранного текста
+            var currentValue = richTextBox.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            if (currentValue != DependencyProperty.UnsetValue && currentValue.Equals(FontWeights.Bold))
+            {
+                richTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Normal);
+            }
+            else
+            {
+                richTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Bold);
+            }
+        }
+        private void Underline_Click(object sender, RoutedEventArgs e)
+        {
+            // Переключить подчеркивание для выбранного текста
+            var currentValue = richTextBox.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            if (currentValue != DependencyProperty.UnsetValue && currentValue.Equals(TextDecorations.Underline))
+            {
+                richTextBox.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, null);
+            }
+            else
+            {
+                richTextBox.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
+            }
+        }
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Создатель: Шмидт Антон, поддержите юного таланта хорошей оценкой, его будущее в ваших руках!");
+        }
+    }
+}
