bbc208bb7abb60ef759c841806b5869339f04f5b
Антон Шмидт <sparda89328490316@mail.ru>
2023-05-13 16:41:28 +0500

    Pz_Delegate

---
+++ Pz_Delegate/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Pz_Delegate
+{
+    delegate string TestDelegate();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Group = "а";
+            Pupil pupil = new Pupil();
+            pupil.Klas = "7";
+            TestDelegate testDelegate = new TestDelegate(pupil.GetKlas);
+            testDelegate += student.GetGroup;
+            foreach (TestDelegate method in testDelegate.GetInvocationList())
+            {
+                Console.Write(method());
+            }
+        }
+    }
+    internal class Student
+    {
+        private string group;
+        public string Group { set { group = value; } }
+        public string GetGroup()
+        {
+            return group;
+        }
+    }
+    class Pupil
+    {
+        private string klas;
+        public string Klas { set { klas = value; } }
+        public string GetKlas()
+        {
+            return klas;
+        }
+    }
+}
